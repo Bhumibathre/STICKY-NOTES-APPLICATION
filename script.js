82a2c2816e24362656fe9df6efcfcced47e0a788
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2026-05-03 13:41:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addBtn = document.querySelector("#addBtn");
 const main = document.querySelector("#main");
 
-addBtn.addEventListener("click", addNote);
+addBtn.addEventListener("click", addNote); 
 
 function addNote() {
   const note = document.createElement("div");
